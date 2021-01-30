76cfc62a7cb563f18271269b1d94d85e5efe489d
Jan Pierre <gamunaxx@gmail.com>
2021-01-29 19:04:22 -0500

    feat: implement cache

--- src/app/core/facades/infographic.facade.ts
+++ src/app/core/facades/infographic.facade.ts
@@ -4,13 +4,17 @@ import { InfographicService } from '../services/infographic.service';
 import { map, mergeMap } from 'rxjs/operators';
 import { Tag } from 'src/app/shared/models/tag';
 import { Infographic } from 'src/app/shared/models/infographic';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { TagFacade } from './tag.facade';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfographicFacade {
+  responseCache = new Map();
+  responseCacheTag = new Map();
+  cacheMainInfographic = 'mainInfographic';
+  cacheInfographiTag = 'infographicTag';
 
   constructor(
     private infographicService: InfographicService,
@@ -18,19 +22,26 @@ export class InfographicFacade {
   ) { }
 
   getSectionByTagMain(): Observable<any> {
+    const mainInfographics = this.responseCache.get(this.cacheMainInfographic);
+    if (mainInfographics) {
+      return of(mainInfographics);
+    }
+
     return this.tagFacade.getTags()
       .pipe(
         mergeMap((tags: Tag[]) => {
           return this.infographicService.getInfographicsMain()
             .pipe(
               map((infographics: Infographic[]) => {
-               return tags
-                .sort((a, b) => (Number(a.order) - Number(b.order)))
-                .map((item: Tag) => {
-                  const filter = infographics
-                    .filter(infographic => this.searchTag(infographic?.tags, item?.id));
-                  return filter?.length > 0 && { [item?.name]: filter };
-                }).filter(item => item);
+                const result = tags
+                  .sort((a, b) => (Number(a.order) - Number(b.order)))
+                  .map((item: Tag) => {
+                    const filter = infographics
+                      .filter(infographic => this.searchTag(infographic?.tags, item?.id));
+                    return filter?.length > 0 && { [item?.name]: filter };
+                  }).filter(item => item);
+                this.responseCache.set(this.cacheMainInfographic, result)
+                return result;
               })
             );
         })
@@ -38,6 +49,11 @@ export class InfographicFacade {
   }
 
   getSectionByTag(tagName: string): Observable<any> {
+    const infographicsTags = this.responseCacheTag.get(tagName);
+    if (infographicsTags) {
+      return of(infographicsTags);
+    }
+
     return this.tagFacade.getTags()
       .pipe(
         mergeMap((tags: Tag[]) => {
@@ -45,12 +61,15 @@ export class InfographicFacade {
           return this.infographicService.getInfographicsByTag(idTag?.id)
             .pipe(
               map((infographics: Infographic[]) => {
-                return tags.map((item: Tag) => {
+                const result = tags.map((item: Tag) => {
                   const filter = infographics
                     .filter(infographic => this.searchTag(infographic?.tags, item?.id));
                   return filter?.length > 0 && { [item?.name]: filter };
                 }).filter(item => item)
                   .sort((a, b) => (Number(a.order) - Number(b.order)));
+
+                this.responseCacheTag.set(tagName, result);
+                return result;
               })
             );
         })
